6155d600b154c2b41f5e37c8128e77daa70533b2
Ba9A-tata <yogadzakkii@gmail.com>
2025-11-01 12:58:19 +0700

    menambahkan var nama

--- app/src/main/java/com/example/userinput_praktikum/TeksEdit.kt
+++ app/src/main/java/com/example/userinput_praktikum/TeksEdit.kt
@@ -1,9 +1,20 @@
 package com.example.userinput_praktikum
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-
-fun FormPendaftaran() {
+fun FormPendaftaran(modifier: Modifier) {
+    var nama by remember { mutableStateOf("") }
 
 }
